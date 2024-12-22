852496f3f08e979ae096a267acc91c017ab58fb1
GustavoToebe <gustavotoebe4@gmail.com>
2024-12-22 18:11:37 -0300

    Ajustando e criando o endpoint nos controllers

--- src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesController.java
@@ -2,12 +2,30 @@ package dev.java10x.cadastrodeninjas.Missoes;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("missoes")
 public class MissoesController {
 
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missao criado com sucesso!";
+    }
+
+    @PutMapping("/atualizarMissao")
+    public String atualizarMissao(){
+        return "Missao atualizado com sucesso!";
+    }
+
+    @GetMapping("/mostrarTodasAaMissoes")
+    public String listarMissoes(){
+        return "Lista de missoes";
+    }
+
+    @DeleteMapping("/deletarMissao")
+    public String deletarMissao(){
+        return "Deletado com sucesso!";
+    }
 
 }
--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjasController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjasController.java
@@ -1,16 +1,40 @@
 package dev.java10x.cadastrodeninjas.Ninjas;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninja")
 public class NinjasController {
 
     @GetMapping("/boasvindasninja")
     public String boasVindas() {
         return "Boa vindas ninjas";
     }
+
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Criado com sucesso";
+    }
+
+    @GetMapping("/todos")
+    public String mostrarNinjas() {
+        return "Lista de ninjas";
+    }
+
+    @GetMapping("/todosID")
+    public String mostrarNinjasById() {
+        return "Lista de ninjas por ID";
+    }
+
+    @PutMapping("/alteraID")
+    public String atualizarNinjasById() {
+        return "Atualizado com sucesso";
+    }
+
+    @DeleteMapping("/exxluirID")
+    public String excluirNinjas() {
+        return "Excluido com sucesso";
+    }
+
 }
